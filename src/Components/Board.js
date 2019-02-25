6837c31bfba5c471b7ae3aab436433160fceb4bb
raechel <odom.raechel@gmail.com>
2019-02-24 23:49:36 -0700

    Components have been updated with additional components for better functionality. Their Sass file counterparts and testing files have been added as well

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -4,16 +4,43 @@ import Card from './Card';
 
 
 export default class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      questions: this.props.questions,
+      currQ: {},
+      answerBank: [],
     }
   }
 
+  shuffleQs = () => {
+    let currentQ = this.state.questions.sort(() => 0.5 - Math.random()).pop()
+    this.setState( {currQ: currentQ} )
+  }
+
+  shuffleAs = () => {
+    let ans = this.state.questions.reduce((acc, q) => {
+      acc.push(q.answer)
+      return acc
+    }, [])
+    let shuffledAs = ans.sort(() => 0.5 - Math.random())
+    this.setState( {answerBank: shuffledAs} )
+  }
+
   render() {
-    return <div className="card-board">
-            <Card />
-            <PlayerScore />
-          </div>
+    return (
+        <div className="card-board">
+          <Card q={this.state.currQ} 
+          nextQ={this.shuffleQs} 
+          ansBank={this.state.answerBank} 
+          cq={this.props.cq}
+          iq={this.props.iq}
+          />
+          <button 
+          onClick={this.shuffleAs}>
+          Start Game</button>
+          <PlayerScore />
+        </div>
+    )
   }
 }
---
+++ src/Tests/Board.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Board from '../Components/Board';
+import { shallow } from 'enzyme';
+
+describe('Board', () => {
+
+  let wrapper;
+  
+  beforeEach(() => {
+    wrapper = shallow(
+      <Board />
+    )
+  });
+
+  it('should match the snapshot with all the data passed in', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have the proper default state', () => {
+    expect(wrapper.state).toEqual( {
+      questions: [], currQ: {}, answerBank: []
+    })
+  });
+})
